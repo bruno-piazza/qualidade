80551781fdfb07f16767e59f1495a1ec078d159d
Bruno Piazza <brunocpiazza@gmail.com>
2022-07-20 19:10:31 -0300

    entrega L4

--- 1. Listas de exercício/Lista 04/L4.r
+++ 1. Listas de exercício/Lista 04/L4.r
@@ -1,3 +1,24 @@
-library(qcc)
+rm(list = ls ())
 
-cManpower <- c("Execução das medidas "," Paralaxe "," Erros de recorte ")
+library(qcc)
+library(ggplot2)
+
+cManpower <- c(" Execução das medidas "," Paralaxe "," Erros de recorte ", " Tempo de reflexo ")
+cMaterials <- c( " Baterias " ," Lâmpadas " ," Régua " ," Balança " )
+cMachines <- c( " Baixa precisão da régua " ," Baixa precisão da balança ")
+cMethods <- c( " Medição manual dos comprimentos " ," Padronização das medidas " ," Manuseio dos equipamentos " )
+cMeasurements <- c( " Iluminação ambiente " ," Régua " ," Balança " )
+cEnvironment <- c ( " Temperatura " ," Umidade relativa " ," Localização ")
+
+cEffect <- " Incertezas de medição "
+
+png (file = "diagrama.png" , #nolint
+      width = 1200, height = 500)
+cause.and.effect(cause = list( Mão_de_Obra = cManpower ,
+                Materiais = cMaterials ,
+                Máquinas = cMachines ,
+                Método = cMethods ,
+                Metrologia = cMeasurements ,
+                Meio_Ambiente = cEnvironment ) ,
+  effect = cEffect )
+dev.off()
